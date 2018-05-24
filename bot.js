0572863b4cc5e37c1f0e2154661317bdc644e0ce
D2Radijas <39580726+D2Radijas@users.noreply.github.com>
2018-05-24 09:48:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
+client.on('!ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === '!ping') {
+    	message.reply('over9000');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '!play') {
+    	message.reply('Jeigu radijas nėra "Radijas" kanale, tai radijas išjungtas. Prašome užeiti kitą kartą. Linkiu jums geros dienos!');
   	}
 });
 
